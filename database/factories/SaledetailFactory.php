5b19a01b06f98abc7cd8cc95fad47f24c37bd5c4
Osvaldo <osvaldo.alvarado.dev@gmail.com>
2021-01-23 20:05:42 -0300

    Carga archivos a la repo

---
+++ database/factories/SaledetailFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Saledetail;
+use Faker\Generator as Faker;
+
+$factory->define(Saledetail::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
